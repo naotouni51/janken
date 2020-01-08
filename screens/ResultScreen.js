39cbda64f09934aab9b36062fe47bb8aec7bba5d
naotouni51 <naotouni51@gmail.com>
2020-01-08 15:40:41 +0900

    相手のじゃんけんidを表示"

--- screens/ResultScreen.js
+++ screens/ResultScreen.js
@@ -4,18 +4,22 @@ import { StyleSheet, Text, View } from 'react-native';
 
 class ResultScreen extends React.Component {
   state = {
-    jankenID: 0
+    jankenID: 0,
+    opponentJankenID: 0
   }
 
   componentDidMount() {
+    let tempOpponentJankenID = Math.floor( Math.random() * (2 + 1 - 0) ) + 0 ;
     this.setState({jankenID: this.props.navigation.state.params.jankenID})
+    this.setState({opponentJankenID: tempOpponentJankenID})
   }
 
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center' }}>
-        <Text>ぽい！</Text>
-        <Text>{this.state.jankenID}</Text>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text style={{fontSize: 40}}>ぽい！</Text>
+        <Text style={{fontSize: 40}}>{this.state.jankenID}</Text>
+        <Text style={{fontSize: 40}}>{this.state.opponentJankenID}</Text>
       </View>
     );
   }
